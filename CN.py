aa5d3fbcb11b7bd7f09a8c08316af0bc7430edd0
DIPEDEV <119068138+DIPEDEV@users.noreply.github.com>
2022-12-02 20:29:56 -0600

    Update CN.py

--- CN.py
+++ CN.py
@@ -1,5 +1,15 @@
 # Here i make a console for manage data in a better space
-import bank
+import Bank
 
-class console (bank.bank()):
-  pass
+exit = False
+
+class console (Bank.bank(False)):
+  while not exit:
+    # Preguntaremos que quiere hacer
+    command = input("""
+      What you want to do:
+      1-  Login    # Login for use your money
+      2-  Register # Register for start using your money
+    """)
+    if command == "":
+      pass
